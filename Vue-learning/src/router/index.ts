0d69f52f3ad8f5003847075af68262cf4dfc8eee
royccg <roy778555479@gmail.com>
2024-09-12 15:54:48 +0800

    路由组件和一般组件分开

--- Vue-learning/src/router/index.ts
+++ Vue-learning/src/router/index.ts
@@ -2,9 +2,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 // 引入组件
-import Home from "@/components/Home.vue";
-import News from "@/components/News.vue";
-import About from "@/components/About.vue";
+import Home from "@/pages/Home.vue";
+import News from "@/pages/News.vue";
+import About from "@/pages/About.vue";
 
 
 // 第二步： 创建路由器
