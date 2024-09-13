d0495ae379bcfd565d56e5fb7b8ff9fd91f1b93b
royccg <roy778555479@gmail.com>
2024-09-13 10:17:19 +0800

    编程式路由导航2: router.push

--- Vue-learning/src/pages/News.vue
+++ Vue-learning/src/pages/News.vue
@@ -23,7 +23,7 @@
                 <!-- <RouterLink :to="`/news/detail/${news.id}/${news.title}/${news.content}`">
                     {{ news.title }}
                 </RouterLink> -->
-
+                <button @click="showNewsDetail(news)">News</button>  <!--传入 news -->
                 <RouterLink :to="{
                     name: 'xinwen_detail',
                     query:{
@@ -46,7 +46,7 @@
 
 <script setup lang="ts">
     import { reactive } from "vue";
-    import { RouterView, RouterLink } from "vue-router";
+    import { RouterView, RouterLink, useRouter } from "vue-router";
 
     const newsList = reactive([
         { id: "news01", title: "横号的啊三分大赛", content: "asdfas " },
@@ -54,5 +54,23 @@
         { id: "news03", title: "横号的啊三分大赛123", content: "asdfasasfa" },
         { id: "news04", title: "good news", content: "asdfasdasf" },
     ])
+
+    const router = useRouter()
+    interface NewsInter {
+        id: string,
+        title: string,
+        content: string
+    }
+    function showNewsDetail(news:NewsInter){
+        // router.replace({  //没有记录
+        router.push({
+            name: 'xinwen_detail',
+            query:{
+            id: news.id,
+            title: news.title,
+            content: news.content
+            }
+        })
+    }
 
 </script>
