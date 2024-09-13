073f6d91a2f5858754f448af331001d1b9e62c3d
royccg <roy778555479@gmail.com>
2024-09-13 14:14:47 +0800

    storetoRefs: 解包之后仍然带映射

--- Vue-learning/src/components/Count.vue
+++ Vue-learning/src/components/Count.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="count">
-    <h2>当前求和为：{{ countStore.sum }}</h2>
+    <h2>当前求和为：{{ sum }}</h2>
     <select name="count_opt" id="count-opt" v-model.number="n">
       <option value="1">1</option>
       <option value="2">2</option>
@@ -15,8 +15,11 @@
 <script lang="ts" setup name="Count">
 import { ref } from "vue";
 import { useCountStore } from "@/store/Count";
+import { storeToRefs } from "pinia";
 
 const countStore = useCountStore()
+// storeToRefs 只会关注 store中的数据，不会对方法进行ref包裹
+const {sum} = storeToRefs(countStore)
 // 数据
 let n = ref(1)
 
